962031d9e05641755449209badd71e1b7890a7a7
ferrevdd <ferre.vandendriessche@gmail.com>
2022-08-15 10:45:39 +0200

    added §loading command

--- discordbot/selfbot.py
+++ discordbot/selfbot.py
@@ -3,21 +3,13 @@ from colorama import Fore
 from subprocess import DEVNULL,STDOUT
 import sys
 import discord
-
 from discord.ext import commands
-
-import base64
 import requests
 import asyncio
 def cls():
     __main__.util.cls()
 def mainscrn():
     __main__.init.MainScreen()
-
-
-
-
-
 
 
 def SelfBot(tokywoky,q):
@@ -100,6 +92,20 @@ def SelfBot(tokywoky,q):
                 await asyncio.sleep(10)
                 await asd.delete()
             except:pass
+        
+        @bot.command()
+        async def loading(ctx,object:str,sec:int):
+            await ctx.message.delete()
+            try:
+                msg = await ctx.send('...')
+                for i in range(sec):
+                    await asyncio.sleep(1)
+                    am = round((i+1)/sec*100/5)
+                    am2 = round(20 -(i+1)/sec*100/5)
+                    await msg.edit(content='***'+object+'*** \n'+'['+am*'█'+am2*'▒'+']  ('+str(round((i+1)/sec*100))+'%)' )
+                await msg.edit(content='***'+object+'*** \n'+'[***DONE***]  ('+str(round((i+1)/sec*100))+'%)' )
+            except:
+                pass
 
         #<---starting the bot--->
         try:
@@ -122,6 +128,7 @@ def botinfo():
     print(Fore.LIGHTBLUE_EX+'"ipup" - usage : ipup <ip>, returns info in chat about an ip (ex: ipup 1.1.1.1)')
     print(Fore.LIGHTBLUE_EX+'"pfp" - usage : pfp <@user>, returns profile pic of a user (ex: pfp @CodeSyncio#7302)')
     print(Fore.LIGHTBLUE_EX+'"purge" - usage : purge <purge amount>, deletes a given amount of msges (ex: purge 100)')
+    print(Fore.LIGHTBLUE_EX+'"loading" - usage : loading <what you are doing> <time in seconds>, shows a loading bar loading for the specified amount of time (ex: loading "downloading games" 120 )')
     print(Fore.MAGENTA+"press enter to return to main menu")
     ghinput = input()
     mainscrn()
